05539b86ae5b1bb72b5f0fdf7f881efbda6a8229
Pagi Maula <pagi_maula@aleeas.com>
2022-07-22 19:33:07 +0530

    Day 03: Solved two katas on codewars

---
+++ codewars/kata 03.rb
@@ -0,0 +1,22 @@
+#iven an array of integers your solution should find the smallest integer.
+
+#For example:
+
+#Given [34, 15, 88, 2] your solution will return 2
+#Given [34, -345, -1, 100] your solution will return -345
+#You can assume, for the purpose of this kata, that the supplied array will not be empty.
+
+#solution :
+
+def find_smallest_int(arr)
+  smallest = arr[0]
+  i = 1
+  until i == arr.size do
+    if arr[i] < smallest
+      smallest = arr[i]
+      end
+    i+=1
+    end
+  return smallest
+  #your code here
+end
